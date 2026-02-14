c84d61d869eed4e842d19595c589b0cca14f4d0d
Ivan1990Nik <pelevin.vanyusha90@mail.ru>
2026-02-14 22:10:56 +0300

    comi

--- bot.js
+++ bot.js
@@ -431,7 +431,7 @@ async function dailyNewsTask() {
 // Cron — 2 раза в день
 // ======================
 
-cron.schedule('45 9,15,21 * * *', dailyNewsTask, { timezone: 'Europe/Moscow' });
+cron.schedule('21 9,15,19 * * *', dailyNewsTask, { timezone: 'Europe/Moscow' });
 
 
 // ======================
